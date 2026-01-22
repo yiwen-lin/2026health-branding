6a436669d268ba4318009d8cb93f165725172e13
kohakubaibai <hakupai1990@gmail.com>
2026-01-22 16:55:27 +0800

    0122 fix survey

--- js/chartSet.js
+++ js/chartSet.js
@@ -4,7 +4,7 @@
 	    data: {
 	        labels: ["男", "女"],
 	        datasets: [{
-	            data: [25.5, 74.5],
+	            data: [32.7, 67.3],
 	            backgroundColor: [
 	                '#58AAB3',
 	                '#CEE2E5'
@@ -23,7 +23,7 @@
 	    data: {
 	        labels: ["已婚", "未婚/單身"],
 	        datasets: [{
-	            data: [68.2, 31.8],
+	            data: [64.1, 35.9],
 	            backgroundColor: [
 	                '#FF9B53',
 	                '#FFC121'
@@ -42,14 +42,14 @@
 	    data: {
 	        labels: [
 	        	"25歲以下",
-	        	"25歲~35歲",
-	        	"36歲~45歲",
-	        	"46歲~55歲",
-	        	"56歲~64歲",
+	        	"25歲~34歲",
+	        	"35歲~44歲",
+	        	"45歲~54歲",
+	        	"55歲~64歲",
 	        	"65歲以上"
 	        ],
 	        datasets: [{
-	            data: [1.0, 6.3, 17.3, 30.4, 30.6, 14.4],
+	            data: [2.6, 9.3, 21.1, 31.4, 24.5, 11.0],
 	            backgroundColor: [
 	                '#7F573A',
 	                '#A56D45',
@@ -71,11 +71,11 @@
 	    type: 'doughnut',
 	    data: {
 	        labels: [
-	        	"有",
-	        	"無"
+	        	"無",
+	        	"有"
 	        ],
 	        datasets: [{
-	            data: [65.2, 34.8],
+	            data: [40.8, 59.2],
 	            backgroundColor: [
 	                '#B5A092',
 	                '#DFD7D1'
@@ -100,7 +100,7 @@
 	        	"15萬以上"
 	        ],
 	        datasets: [{
-	            data: [27.7, 29.6, 22.3, 9.1, 11.2],
+	            data: [26.1, 28.7, 22.3, 9.1, 13.8],
 	            backgroundColor: [
 	                '#0071E3',
 	                '#3B90E5',
@@ -129,7 +129,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [31.2, 46.6, 19.4, 2.5, 0.3],
+	            data: [30.7, 41.1, 21.8, 4.2, 2.2],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#77CE7B',
@@ -169,7 +169,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [76.8, 46.9, 34.1, 33.7, 20.5, 18.7, 15.2, 13.1, 0.6],
+	            data: [68.7, 43.3, 35.4, 26.4, 22.1, 19.3, 13.7, 12.4, 0.8],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
@@ -194,7 +194,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [54.4, 39.7, 5.2, 0.6, 0.2],
+	            data: [50.9, 37.5, 7.8, 2.0, 1.7],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#77CE7B',
@@ -220,7 +220,7 @@
 	        	"沒有"
 	        ],
 	        datasets: [{
-	            data: [69.8, 30.2],
+	            data: [59.7, 40.3],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#9EDCA1',
@@ -234,52 +234,22 @@
 	});
 
 
-	// var ctxQ5 = $("#chart-q5");
-	// var myChart = new Chart(ctxQ5, {
-	//     type: 'radar',
-	//     data: {
-	//         labels: [
-	//         	"1",
-	//         	"2",
-	//         	"3",
-	//         	"4",
-	//         	"5",
-	//         	"6",
-	//         ],
-	//         datasets: [{
-	//         	label: '分布比率',
-	//             data: [27.9, 5.9, 26.3, 35.7, 3.0, 1.2],
-	//             backgroundColor: 'rgba(119, 206, 123, 0.8)',
-	//             borderColor: '#77CE7B',
-	//             borderWidth: 1
-	//         }]
-	//     },
-	//     options: {
-	//     	responsive: true,
-	//     }
-	// });
 	var ctxQ5 = $("#chart-q5");
 	var myChart = new Chart(ctxQ5, {
 	    type: 'doughnut',
 	    data: {
 	        labels: [
-	        	"吃速食或外賣",
-	        	"隨便吃點零食解決",
-	        	"挑選簡單又健康的輕食",
-	        	"儘量自己煮簡單的家常菜",
-				"有時會忘記吃飯",
-				"其他"
+	        	"太胖/有點胖",
+	        	"我的體態很剛好",
+	        	"太瘦/有點瘦",
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [27.9, 5.9, 26.3, 35.7, 3.0, 1.2],
+	            data: [63.4, 27.5, 9.2],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#77CE7B',
 	                '#9EDCA1',
-	                '#BBE7BD',
-	                '#E2F5E3',
-					'#EDFDEE'
 	            ],
 	            borderWidth: 1
 	        }]
@@ -295,19 +265,17 @@
 	    type: 'doughnut',
 	    data: {
 	        labels: [
-	        	"是，經常感覺不均衡",
-	        	"偶爾感覺有點不均衡",
-	        	"不太確定",
-	        	"否，我的飲食習慣很均衡",
+	        	"正在積極執行",
+	        	"有需求但還沒開始",
+	        	"沒有需求",
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [26.8, 55.2, 6.8, 11.2],
+	            data: [26.8, 44.2, 29.0],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#77CE7B',
 	                '#9EDCA1',
-	                '#BBE7BD',
 	            ],
 	            borderWidth: 1
 	        }]
@@ -332,7 +300,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [39.3, 46.6, 69.3, 7.0, 9.1, 0.5],
+	            data: [51.4, 59.3, 46.8, 49.7, 12.3, 3.5],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
@@ -343,36 +311,8 @@
 	    }
 	});
 
-
 	var ctxQ8 = $("#chart-q8");
 	var myChart = new Chart(ctxQ8, {
-	    type: 'doughnut',
-	    data: {
-	        labels: [
-	        	"非常願意購買",
-	        	"可能會嘗試購買",
-	        	"不確定，需更多了解",
-	        	"不願意購買"
-	        ],
-	        datasets: [{
-	            data: [15.8, 53.4, 27.5, 3.2],
-	            backgroundColor: [
-	                '#3DB943',
-	                '#77CE7B',
-	                '#9EDCA1',
-	                '#BBE7BD',
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	    	responsive: true,
-	    }
-	});
-
-
-	var ctxQ9 = $("#chart-q9");
-	var myChart = new Chart(ctxQ9, {
 	    type: 'radar',
 	    data: {
 	        labels: [
@@ -382,11 +322,12 @@
 	        	"D",
 	        	"E",
 	        	"F",
-	        	"G"
+				"G",
+				"H"
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [ 59.1, 25.1, 18.5, 38.2, 49.0, 43.3, 0.7],
+	            data: [61.6, 25.2, 14.9, 53.7, 29.0, 11.6, 6.7, 4.4],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
@@ -397,23 +338,75 @@
 	    }
 	});
 
+	var ctxQ9 = $("#chart-q9");
+	var myChart = new Chart(ctxQ9, {
+	    type: 'doughnut',
+	    data: {
+	        labels: [
+	        	"均衡飲食，不挨餓",
+	        	"提升代謝，減少脂肪囤積",
+	        	"維持肌肉量，避免虛弱",
+				"搭配保健品，輔助減重",
+				"短期快速見效",
+	        ],
+	        datasets: [{
+	        	label: '分布比率',
+	            data: [29.0, 39.7, 23.4, 5.0, 2.9],
+	            backgroundColor: [
+					'#3DB943',
+	                '#77CE7B',
+	                '#9EDCA1',
+	                '#BBE7BD',
+	                '#E2F5E3',
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	    	responsive: true,
+	    }
+	});
 
 	var ctxQ10 = $("#chart-q10");
 	var myChart = new Chart(ctxQ10, {
-	    type: 'bar',
+	    type: 'doughnut',
 	    data: {
 	        labels: [
-	        	"1",
-	        	"2",
-	        	"3",
-	        	"4",
-	        	"5",
-	        	"6",
-	        	"7"
+	        	"有，持續使用中",
+	        	"有，但已停用",
+	        	"沒有",
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [19.3, 29.7, 10.7, 11.9, 13.9, 12.6, 1.7],
+	            data: [17.5, 19.6, 62.8],
+	            backgroundColor: [
+					'#3DB943',
+	                '#77CE7B',
+	                '#9EDCA1',
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	    	responsive: true,
+	    }
+	});
+
+	var ctxQ11 = $("#chart-q11");
+	var myChart = new Chart(ctxQ11, {
+	    type: 'radar',
+	    data: {
+	        labels: [
+	        	"A",
+	        	"B",
+	        	"C",
+	        	"D",
+	        	"E",
+	        	"F",
+	        ],
+	        datasets: [{
+	        	label: '分布比率',
+	            data: [26.3, 20.7, 19.4, 35.7, 52.0, 25.2],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
@@ -425,8 +418,8 @@
 	});
 
 
-	var ctxQ11 = $("#chart-q11");
-	var myChart = new Chart(ctxQ11, {
+	var ctxQ12 = $("#chart-q12");
+	var myChart = new Chart(ctxQ12, {
 	    type: 'bar',
 	    data: {
 	        labels: [
@@ -439,7 +432,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [26.6, 54.4, 15.7, 1.9, 1.1, 0.2],
+	            data: [42.2, 39.0, 11.1, 2.7, 2.0, 2.9],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#77CE7B',
@@ -447,29 +440,6 @@
 	                '#BBE7BD',
 	                '#E2F5E3',
 					'#EDFDEE'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	    	responsive: true,
-	    }
-	});
-
-
-	var ctxQ12 = $("#chart-q12");
-	var myChart = new Chart(ctxQ12, {
-	    type: 'doughnut',
-	    data: {
-	        labels: [
-	        	"是",
-	        	"否"
-	        ],
-	        datasets: [{
-	            data: [88.0, 12.0],
-	            backgroundColor: [
-	                '#3DB943',
-	                '#9EDCA1'
 	            ],
 	            borderWidth: 1
 	        }]
@@ -497,7 +467,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [60.3, 49.6, 17.8, 34.6, 21.5, 57.5, 44.7, 27.7, 2.3],
+	            data: [48.2, 45.4, 29.9, 26.5, 51.9, 37.5, 30.5, 2.2],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
@@ -531,7 +501,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [43.1, 33.6, 29.6, 9.5, 10.3, 14.1, 11.9, 31.9, 43.3, 48.3, 39.0, 57.3, 21.7, 4.0],
+	            data: [39.3, 37.0, 26.3, 11.4, 13.7, 17.3, 16.7, 30.7, 38.8, 46.4, 42.1, 27.3, 5.4],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
@@ -557,7 +527,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [27.2, 51.3, 16.8, 2.7, 1.1, 0.9],
+	            data: [31.3, 45.4, 16.3, 3.6, 2.3, 1.2],
 	            backgroundColor: [
 	                '#3DB943',
 	                '#77CE7B',
@@ -590,7 +560,7 @@
 	        ],
 	        datasets: [{
 	        	label: '分布比率',
-	            data: [52.3, 29.2, 2.3, 55.4, 28.2, 8.1,5.8 ],
+	            data: [52.6, 23.1, 3.9, 49.0, 26.0, 10.8, 5.6],
 	            backgroundColor: 'rgba(119, 206, 123, 0.8)',
 	            borderColor: '#77CE7B',
 	            borderWidth: 1
